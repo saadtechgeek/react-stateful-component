1f3502f3efac3e57dc1fcb57923642a5c7b85473
Saad Shahid <saad.techgeek@gmail.com>
2020-09-02 15:17:14 +0500

    state example commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {Message} from './Message.js'
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
+  let [count, setCount] = useState(1);
+  let [isMorning,setMorning] = useState(false);
+  //count = 1;
+  //setCount(2) // always setter
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Day time = {isMorning ? 'Morning' : 'Night'}</h1>
+        Value of counter variable is {count}
+        <br/>
+        Message From component <br/>
+        <Message counter={count}/>
+        <br/>
+        <button onClick={()=>setCount(++count)}>Updater counter</button>
+        <button onClick={()=>setMorning(!isMorning)}>Updater day</button>
       </div>
     );
-  }
 }
 
 export default App;
